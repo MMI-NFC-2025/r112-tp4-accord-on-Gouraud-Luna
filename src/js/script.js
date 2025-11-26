2079a6d3afab6f8222427e747bc7e86e795f4c21
Gouraud Luna <luna.gouraud@edu.univ-fcomte.fr>
2025-11-26 11:11:23 +0100

    display flex accordeon parent

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
 // *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+accordeon.forEach(accordeon => {
+    accordeon.addEventListener("click", (event) => {
+        accordeon.querySelectorAll("details").forEach((det) => (det.open = false)))
